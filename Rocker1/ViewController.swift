ccd597d5cb60bde6b01941a4eec3fd53ecca6d2d
Kevin Hankens <email@kevinhankens.com>
2014-07-13 12:53:58 -0400

    Initial Commit

---
+++ Rocker1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rocker1
+//
+//  Created by Kevin Hankens on 7/13/14.
+//  Copyright (c) 2014 Kevin Hankens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
